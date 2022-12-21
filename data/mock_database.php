08138092c76de3a943d689b761767b913f7cc10a
Alex Van der velden <a.vandervelden3@student.avans.nl>
2022-12-21 10:46:13 +0100

    setting up map structure

--- data/mock_database.php
+++ data/mock_database.php
@@ -1,5 +1,12 @@
 <?php
 
+$data = [
+    ['accountID' => 'TAID63a1ccc382267', 'email' => 'testeamail1@email.com', 'password' => 'secret01!', 'enabled' => true, 'roles' => [], 'isLoggedIn' => false],
+    ['accountID' => 'TAID52b2ddd234751', 'email' => 'testeamail2@email.com', 'password' => 'secret02!', 'enabled' => true, 'roles' => [], 'isLoggedIn' => false],
+    ['accountID' => 'TAID41c3eee256863', 'email' => 'testeamail3@email.com', 'password' => 'secret03!', 'enabled' => true, 'roles' => [], 'isLoggedIn' => false],
+];
+
+var_dump($data);
 
 
 ?>
