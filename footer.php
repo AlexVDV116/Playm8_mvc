562c1ed187c6cb5adf235d891930ae233e57023b
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-01-31 18:25:53 +0100

    added Github URL in footer

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
                 <ul class="social-icons">
                     <li><a href="https://instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
                     <li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="https://github.com/" target="_blank"><i class="fa fa-github"></i></a></li>
+                    <li><a href="https://github.com/playm8-avans" target="_blank"><i class="fa fa-github"></i></a></li>
                     <li><a href="<?php echo $ROOT; ?>view/contact.php"><i class="fa-regular fa-envelope"></i></a>
                     </li>
                 </ul>
