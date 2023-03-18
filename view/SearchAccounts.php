b984858c3b17786f724c0168259d1a91b66bb29f
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-03-18 18:11:07 +0100

    created a searchAccounts.php

---
+++ view/SearchAccounts.php
@@ -0,0 +1,63 @@
+<?php
+require_once 'framework/View.php';
+require_once 'model/Account.php';
+require_once 'dao/accountDAO.php';
+
+ini_set('display_errors', 1);
+
+class SearchAccounts extends View
+{
+
+    public function show()
+    {
+        $accountDAO = new accountDAO;
+        $accountDAO->startList();
+?>
+
+        <form action="phpSearch.php" method="post">
+            Search <input type="text" name="search"><br>
+            <input type="submit">
+        </form>
+
+        <div class="class-responsive">
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Account ID</th>
+                        <th>Naam</th>
+                        <th>Email</th>
+                        <th>Enabled</th>
+                        <th>Beta</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($accountDAO->hasNext()) {
+                        $account = $accountDAO->getNext();
+                    ?>
+                        <tr onclick="">
+                            <!-- PHP shorthand to echo the data in the table -->
+                            <td><?= '# ' . $account->getAccountID() ?></td>
+                            <td><?= $account->getName() ?></td>
+                            <td><?= $account->getEmail() ?></td>
+                            <td><?php if ($account->getEnabled()) {
+                                    echo 'Yes';
+                                } else {
+                                    echo 'No';
+                                } ?></td>
+                            <td><?php if ($account->getBetaUser()) {
+                                    echo 'Yes';
+                                } else {
+                                    echo 'No';
+                                } ?>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+<?php
+    }
+}
+new SearchAccounts;
