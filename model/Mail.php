8526ea1c12200241b1a1234925dc2e91fee45884
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-04-23 22:23:19 +0200

    disabled SMTP debug

--- model/Mail.php
+++ model/Mail.php
@@ -31,7 +31,7 @@ class Mail extends Model
         $mail = new PHPMailer;
 
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                          //Enable verbose debug output
+        $mail->SMTPDebug = 0;                          //Enable verbose debug output
         $mail->isSMTP();                                                //Send using SMTP
         $mail->Host       = mailConfig::CONFIG['email']['host'];        //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                       //Enable SMTP authentication
