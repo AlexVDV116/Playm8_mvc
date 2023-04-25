18a9a1139f1b8b7d6da4aa97ac7afd72834cf938
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-04-25 10:39:51 +0200

    created forgotPassword.inc.php

---
+++ includes/forgotPassword.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+// PHP script that grabs the data from the form
+// Instantiates the Controller that runs the server side validations
+// If there are no errors in the controller script redirect user 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_POST["submit"])) {
+
+    // Grabbing the data
+    $email = $_POST["email"];
+
+    // Instantiate the forgotPasswordController class
+    include "../framework/databaseHandler.php";
+    include "../controller/forgotPasswordController.php";
+    $forgot = new forgotPasswordController($email);
+
+    // Running server side validation, error handling and reset the password
+    $forgot->run();
+
+    // Redirect user back to the front page when sucsessfull
+    header("location: ../view/forgotPassword.php?forgot=success");
+} else {
+    header("location: ../index.php");
+}
