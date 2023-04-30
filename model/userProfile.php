4f84a0770fb86da8e29a9b8dfc15e19d3a409a2d
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-04-30 16:53:23 +0200

    chngd construct to contact array data

--- model/userProfile.php
+++ model/userProfile.php
@@ -14,15 +14,9 @@ class userProfile extends Model
     private string $dateOfBirth;
     private string $age;
 
-    public function __construct($userProfileID, $firstName,  $lastName, $location, $phoneNumber, $dateOfBirth, $age)
+    public function __construct(?array $data = null)
     {
-        $this->userProfileID = $userProfileID;
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-        $this->location = $location;
-        $this->phoneNumber = $phoneNumber;
-        $this->dateOfBirth = $dateOfBirth;
-        $this->age = $age;
+        parent::__construct($data);
     }
 
     public function getUserProfileID(): string
