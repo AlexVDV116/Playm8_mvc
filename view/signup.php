4fe4db14423f67b18011cc653aab5b89590cd00e
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-07 23:45:50 +0200

    added errormessages

--- view/signup.php
+++ view/signup.php
@@ -39,10 +39,10 @@ class signup extends View
                                             <div class="d-flex flex-row align-items-center mb-4">
                                                 <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                                 <div class="form-outline flex-fill mb-0">
+                                                    <label for="form_username" class="form-label">Gebruikersnaam</label>
                                                     <input id="username" type="text" name="username" class="form-control" value="<?php if (isset($username)) {
                                                                                                                                         echo $username;
                                                                                                                                     } ?>" required>
-                                                    <label for="form_username" class="form-label">Gebruikersnaam</label>
                                                     <div class="invalid-feedback">
                                                         Dit veld is verplicht.
                                                     </div>
@@ -52,10 +52,10 @@ class signup extends View
                                             <div class="d-flex flex-row align-items-center mb-4">
                                                 <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                                 <div class="form-outline flex-fill mb-0">
+                                                    <label for="form_email" class="form-label">E-mailadres</label>
                                                     <input id="form_email" type="email" name="email" class="form-control" value="<?php if (isset($email)) {
                                                                                                                                         echo $email;
                                                                                                                                     } ?>" required>
-                                                    <label for="form_email" class="form-label">E-mailadres</label>
                                                     <div class="invalid-feedback">
                                                         Voer een geldig e-mailadres in.
                                                     </div>
@@ -63,10 +63,10 @@ class signup extends View
                                             </div>
 
                                             <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                                <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                                                 <div class="form-outline flex-fill mb-0">
-                                                    <input id="form_password" type="password" name="password" class="form-control" required>
                                                     <label for="form_password" class="form-label">Wachtwoord</label>
+                                                    <input id="form_password" type="password" name="password" class="form-control" required>
                                                     <div class="invalid-feedback">
                                                         Dit veld is verplicht.
                                                     </div>
@@ -76,8 +76,8 @@ class signup extends View
                                             <div class="d-flex flex-row align-items-center mb-4">
                                                 <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                                                 <div class="form-outline flex-fill mb-0">
-                                                    <input id="form_passwordrepeat" type="password" name="passwordrepeat" class="form-control" required>
                                                     <label for="form_passwordrepeat" class="form-label">Herhaal uw wachtwoord</label>
+                                                    <input id="form_passwordrepeat" type="password" name="passwordrepeat" class="form-control" required>
                                                     <div class="invalid-feedback">
                                                         Dit veld is verplicht.
                                                     </div>
@@ -117,7 +117,13 @@ class signup extends View
                                                 if ($_GET["activation"] == "fail") {
                                                     echo '<p class="form-error"><i class="fa-solid fa-circle-exclamation"></i> Account activatie mislukt, registreer opnieuw.</p>';
                                                 }
-                                            } ?>
+                                            }
+                                            if (isset($_GET["deleteAccount"])) {
+                                                if ($_GET["deleteAccount"] == "success") {
+                                                    echo '<p class="form-success"><i class="fa-solid fa-circle-exclamation"></i> Account succesvol verwijdert.</p>';
+                                                }
+                                            }
+                                            ?>
                                         </form>
                                     </div>
                                     <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
