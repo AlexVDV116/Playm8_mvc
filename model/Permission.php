60fbdc41251466fc6c15a5401df425f94f9e9f94
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-10 23:02:57 +0200

    added namespace

--- model/Permission.php
+++ model/Permission.php
@@ -1,17 +1,39 @@
 <?php
 
-// Permission class
+// Define the namespace of this class
+namespace Model;
 
-require_once '../framework/Model.php';
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require '../vendor/autoload.php';
+
+// Import classes this class depends on
+use Framework\Model;
+
+// Permission class with several methods to get its own attribute values
 
 class Permission extends Model
 {
-    private string $permissionName;
-    private string $permissionDescription;
+    public int $permissionID;
+    public string $permissionName;
+    public string $permissionDescription;
 
-    public function __construct($permissionName, $permissionDescription)
+    public function __construct(?array $data = null)
     {
-        $this->permissionName = $permissionName;
-        $this->permissionDescription = $permissionDescription;
+        parent::__construct($data);
+    }
+
+    public function getPermissionID(): int
+    {
+        return $this->permissionID;
+    }
+
+    public function getPermissionName(): string
+    {
+        return $this->permissionName;
+    }
+
+    public function getPermissionDescription(): string
+    {
+        return $this->permissionDescription;
     }
 }
