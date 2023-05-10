5492e31dfd446cbe390cd480ee7ce950d27f8df6
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-10 23:02:08 +0200

    created

---
+++ includes/adminDeleteRole.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+// Define the namespace of this class
+namespace Includes;
+
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require '../vendor/autoload.php';
+
+// Import classes this class depends on
+use Controller\roleController;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+if (isset($_POST["submit"])) {
+
+    // Grabbing the new data, use htmlspecialchars to convert user input to html entities including single and double quotes
+    $roleID = $_POST["roleID"];
+    $adminEmail = $_SESSION["auth_user"]["email"];
+    $adminPassword = $_POST["form_adminPassword"];
+
+    // Instantiate the roleController class
+    $roleController = new roleController();
+    $roleController->adminDeleterole($roleID, $adminEmail, $adminPassword);
+}
