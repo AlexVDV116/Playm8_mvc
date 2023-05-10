f3b660722a75a37f51dd8f56673916bc7e6f70c0
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-10 23:05:42 +0200

    created class

---
+++ view/listRolesPermissions.php
@@ -0,0 +1,122 @@
+<?php
+
+// Define the namespace of this class
+namespace View;
+
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require '../vendor/autoload.php';
+
+// Import classes this class depends on
+use Framework\View;
+use DAO\roleDAO;
+use DAO\permissionDAO;
+
+// listRolesPermissions class that lists all roles in the roles table and all permissions from the permissions table 
+// and allows the administrator to edit them
+
+class listRolesPermissions extends View
+{
+
+    public function show()
+    {
+        $roleDAO = new roleDAO;
+        $roleDAO->startList();
+
+        if (isset($_GET["deleteRole"])) {
+            if ($_GET["deleteRole"] == "success") {
+                echo '<p class="form-success"><i class="fa-solid fa-circle-exclamation"></i> Rol succesvol verwijderd.</p>';
+            }
+        }
+        if (isset($_GET["deletePermission"])) {
+            if ($_GET["deletePermission"] == "success") {
+                echo '<p class="form-success"><i class="fa-solid fa-circle-exclamation"></i> Permissie succesvol verwijderd.</p>';
+            }
+        }
+?>
+        <div class="class-responsive">
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Rol ID</th>
+                        <th>Rol naam</th>
+                        <th>Rol beschrijving</th>
+                        <th>Permissies</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // Save all records to a recordArray
+
+                    $recordArray = [];
+                    while ($roleDAO->hasNext()) {
+                        $record = $roleDAO->getNext();
+                        $recordArray[] = $record;
+                    }
+
+                    // Now that we have all records saved in an array we close the connection
+                    $roleDAO->closeConnection();
+
+                    // So we can execute another query and get the permissions belonging to this role
+                    foreach ($recordArray as $record) {
+                        $permission =  $roleDAO->getRolePermissions($record->getRoleID());
+                    ?>
+                        <tr onclick="">
+                            <!-- PHP shorthand to echo the data in the table -->
+                            <td><?= $record->getRoleID()  ?></td>
+                            <td><?= $record->getRoleName() ?></td>
+                            <td><?= $record->getRoleDescription() ?></td>
+                            <td><?= implode(", ", $permission); ?></td>
+                            <td><?= "<a href ='../view/admin.php?view=adminEditRole&roleID=" . $record->getRoleID() . "'>Wijzig" ?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                    <tr>
+                        <td colspan="5"><a href="../view/admin.php?view=addRole"><i class="fa-solid fa-plus"></i> </a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <?php
+        $roleDAO->closeConnection();
+        $permissionDAO = new permissionDAO;
+        $permissionDAO->startList();
+        ?>
+
+        <div class="class-responsive mt-5">
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Permissie ID</th>
+                        <th>Permissie naam</th>
+                        <th>Permissie beschrijving</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // While we have another object in our query
+                    while ($permissionDAO->hasNext()) {
+                        // Fetch the next object and return the current object
+                        $permission = $permissionDAO->getNext();
+                    ?>
+                        <tr onclick="">
+                            <!-- PHP shorthand to echo the data in the table -->
+                            <td><?= "# " . $permission->getPermissionID() ?></td>
+                            <td><?= $permission->getPermissionName() ?></td>
+                            <td><?= $permission->getPermissionDescription() ?></td>
+                            <td><?= "<a href ='../view/admin.php?view=adminEditPermission&permissionID=" . $permission->getPermissionID() . "'>Wijzig" ?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                    <tr>
+                        <td colspan="4"><a href="../view/admin.php?view=addPermission"><i class="fa-solid fa-plus"></i></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+<?php
+    }
+}
+new listRolesPermissions;
