3871785c71b339a803c223b9a436dbd3a3d42ee7
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-10 23:04:56 +0200

    added namespace, created class

--- view/Home.php
+++ view/Home.php
@@ -1,2 +1,26 @@
-<h4>Admin Home</h4>
-<p>Selecteer een van de administrator functionaliteiten.</p>
+<?php
+
+// Define the namespace of this class
+namespace View;
+
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require '../vendor/autoload.php';
+
+// Import classes this class depends on
+use Framework\View;
+
+// Home class that displays various messages to the administrator.
+
+class home extends View
+{
+
+    public function show()
+    {
+?>
+        <h4>Admin Home</h4>
+        <p>Selecteer een van de administrator functionaliteiten.</p>
+
+<?php
+    }
+}
+new home;
