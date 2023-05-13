567d7ee285058f7e166033a2a9e42aacbd8926a2
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-13 19:31:42 +0200

    created a listBetaTesters.php view

---
+++ view/listBetaTesters.php
@@ -0,0 +1,84 @@
+<?php
+
+// Define the namespace of this class
+namespace View;
+
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require '../vendor/autoload.php';
+
+// Import classes this class depends on
+use Framework\View;
+use DAO\accountDAO;
+
+// listAccounts class that lists all accounts in the accounts table and gives the administrator the option to edit each one of them
+
+class listBetaTesters extends View
+{
+
+    public function show()
+    {
+        $accountDAO = new accountDAO;
+        $betaCount = $accountDAO->getBetaCount();
+        $accountDAO->startListBeta();
+
+        if (isset($_GET["addBeta"])) {
+            if ($_GET["addBeta"] == "success") {
+                echo '<p class="form-success"><i class="fa-solid fa-circle-exclamation"></i> Account succesvol verwijderd.</p>';
+            }
+        }
+        if (isset($_GET["removeBeta"])) {
+            if ($_GET["removeBeta"] == "success") {
+                echo '<p class="form-success"><i class="fa-solid fa-circle-exclamation"></i> Account succesvol toegevoegd.</p>';
+            }
+        }
+?>
+        <div>
+            <p>Total number of beta users: <?= $betaCount ?></p>
+        </div>
+
+        <div class="class-responsive">
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Account ID</th>
+                        <th>Userprofile ID</th>
+                        <th>Naam</th>
+                        <th>Email</th>
+                        <th>Actief</th>
+                        <th>Beta-user</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($accountDAO->hasNext()) {
+                        $account = $accountDAO->getNext();
+                    ?>
+                        <tr onclick="">
+                            <!-- PHP shorthand to echo the data in the table -->
+                            <td><?= "# " . $account->getAccountID() ?></td>
+                            <td><?= '# ' . $account->getUserProfileID() ?></td>
+                            <td><?= $account->getUsername() ?></td>
+                            <td><?= $account->getEmail() ?></td>
+                            <td><?php if ($account->getActive()) {
+                                    echo 'Ja';
+                                } else {
+                                    echo 'Nee';
+                                } ?></td>
+                            <td><?php if ($account->getBetaUser()) {
+                                    echo 'Ja';
+                                } else {
+                                    echo 'Nee';
+                                } ?>
+                            <td><?= "<a href ='../view/admin.php?view=adminEditAccount&account=" . $account->getEmail() . "'>Wijzig" ?></td>
+
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+<?php
+    }
+}
+new listBetaTesters;
