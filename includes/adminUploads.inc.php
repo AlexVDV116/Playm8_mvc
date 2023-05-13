802e964550dbbbfcddfc72fe99531e8758232cac
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-13 23:28:14 +0200

    created adminUploads.inc.php

---
+++ includes/adminUploads.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+// Define the namespace of this script
+namespace Includes;
+
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require '../vendor/autoload.php';
+
+// Import classes this script depends on
+use Controller\fileController;
+
+// Include script that handles the user pictures upload
+
+session_start();
+$userProfileID = $_SESSION["adminEditUserProfile"]["userProfileID"];
+
+if (isset($_POST["submit"])) {
+
+    // Grab the data of the file
+    $file = $_FILES["file"];
+    $fileName = $_FILES["file"]["name"];
+    $fileTmpName = $_FILES["file"]["tmp_name"];
+    $fileSize = $_FILES["file"]["size"];
+    $fileType = $_FILES["file"]["type"];
+    $fileError = $_FILES["file"]["error"];
+
+    $fileController = new fileController($userProfileID, $file);
+    $fileController->run();
+}
