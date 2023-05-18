8ef7d0818934655094f3d271a508a927abcdeea8
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-18 19:34:22 +0200

    removed get methods, added get/set method

--- model/Account.php
+++ model/Account.php
@@ -14,64 +14,29 @@ use Framework\Model;
 class Account extends Model
 {
 
-    public string $accountID;
-    public string $username;
-    public string $email;
-    public string $password;
-    public bool $isBetaUser;
-    public bool $isActive;
-    public string $activationCode;
-    public string $activationExpiry;
-    public ?string $activatedAtt;
-    public ?string $userProfileID;
+    private string $accountID;
+    private string $username;
+    private string $email;
+    private string $password;
+    private bool $isBetaUser;
+    private bool $isActive;
+    private string $activationCode;
+    private string $activationExpiry;
+    private ?string $activatedAtt;
+    private ?string $userProfileID;
 
     public function __construct(?array $data = null)
     {
         parent::__construct($data);
     }
 
-    public function getAccountID(): string
+    public function get(mixed $attribute): mixed
     {
-        return $this->accountID;
+        return $this->$attribute;
     }
 
-    public function getUsername(): string
+    public function set(mixed $attribute, $value): void
     {
-        return $this->username;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function getActive(): bool
-    {
-        return $this->isActive;
-    }
-
-    public function getBetaUser(): bool
-    {
-        return $this->isBetaUser;
-    }
-
-    public function getUserProfileID()
-    {
-        return $this->userProfileID;
-    }
-
-    public function getActivationCode(): string
-    {
-        return $this->activationCode;
-    }
-
-    public function getExpiryDate(): string
-    {
-        return $this->activationExpiry;
+        $this->$attribute = $value;
     }
 }
