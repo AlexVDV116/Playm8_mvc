280cecb0ba4cb4bb681f3a5462063a802d14b0b5
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-27 18:57:11 +0200

    added func getAllAttributes

--- model/Role.php
+++ model/Role.php
@@ -4,7 +4,7 @@
 namespace Model;
 
 // Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
-require_once '../vendor/autoload.php';
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 // Import classes this class depends on
 use Framework\Model;
@@ -31,5 +31,10 @@ class Role extends Model
     public function set(mixed $attribute, $value): void
     {
         $this->$attribute = $value;
+    }
+
+    public function getAllAttributes(): array
+    {
+        return get_object_vars($this);
     }
 }
