deb5e6a7699154df712219eb7466f878e6244697
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-29 21:10:46 +0200

    created language file

---
+++ assets/languages/es.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang = [
+    "Home" => "Hogar",
+    "About us" => "Sobre nosotros",
+    "Contact us" => "Contacta con nosotros",
+    "Login" => "Acceso",
+    "Signup" => "Inscribirse",
+    "Hi" => "Hola",
+    "Language" => "Idioma",
+    "Profile" => "Perfil",
+    "Settings" => "Ajustes",
+    "Logout" => "Cerrar sesión",
+
+
+];
