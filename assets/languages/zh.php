49e15ae87ac4d810e1b6a994cf1fe106fe4ddfdc
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-29 21:10:59 +0200

    created language file

---
+++ assets/languages/zh.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang = [
+    "Home" => "家",
+    "About us" => "關於我們",
+    "Contact us" => "聯繫我們",
+    "Login" => "登錄",
+    "Signup" => "登錄",
+    "Hi" => "你好",
+    "Language" => "語言",
+    "Profile" => "輪廓",
+    "Settings" => "設置",
+    "Logout" => "登出",
+
+
+];
