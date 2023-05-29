76d974416096a8dc87fb32584a3ba5423a0cb3a8
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-29 21:13:17 +0200

    fixed header

--- view/login.php
+++ view/login.php
@@ -3,25 +3,46 @@
 // Define the namespace of this class
 namespace View;
 
-// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
-require_once '../vendor/autoload.php';
+/* Echo errors for development purposes */
 
-// Import classes this class depends on
-use Framework\View;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Setting the ROOT directory for this file so the relative paths used in included pages will still work
 $ROOT = '../';
 
-// Include the header
-include_once '../header.php';
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require_once $ROOT . 'vendor/autoload.php';
+
+// Import classes this class depends on
+use Framework\View;
+use View\header;
+use Controller\translatorController;
+
+// Used to translate the header on this page
+$translator = new translatorController;
+// Use the getLanguageFile method of the languageSelector and require the correct language file
+require $ROOT . $translator->getLanguageFile();
 
 // login class that has the login form and echos various error messages after the loginController class validated user input
-
 class login extends View
 {
 
     public function show()
     {
+        $ROOT = '../';
+
+        // Used to translate page content
+        $translator = new translatorController;
+        // Use the getLanguageFile method of the languageSelector and require the correct language file
+        require $ROOT . $translator->getLanguageFile();
+
+        $header = new header();
 ?>
 
         <section>
@@ -33,14 +54,14 @@ class login extends View
                                 <div class="row justify-content-center">
                                     <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                                        <h2 class="text-center mb-5">Inloggen</h2>
+                                        <h2 class="text-center mb-5"><?= $translator->__('Inloggen') ?></h2>
 
                                         <form action="../includes/login.inc.php" method="post" class="needs-validation mx-1 mx-md-4" novalidate>
 
                                             <div class="d-flex flex-row align-items-center mb-4">
                                                 <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                                 <div class="form-outline flex-fill mb-0">
-                                                    <label for="form_email" class="form-label">E-mailadres</label>
+                                                    <label for="form_email" class="form-label"><?= $translator->__('E-mailadres') ?></label>
                                                     <input id="form_email" type="email" name="email" class="form-control" required>
                                                     <div class="invalid-feedback">
                                                         Voer een geldig e-mailadres in.
@@ -51,7 +72,7 @@ class login extends View
                                             <div class="d-flex flex-row align-items-center mb-4">
                                                 <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                                                 <div class="form-outline flex-fill mb-0">
-                                                    <label for="form_password" class="form-label">Wachtwoord</label>
+                                                    <label for="form_password" class="form-label"><?= $translator->__('Wachtwoord') ?></label>
                                                     <input id="form_password" type="password" name="password" class="form-control" required>
                                                     <div class="invalid-feedback">
                                                         Dit veld is verplicht.
@@ -60,11 +81,11 @@ class login extends View
                                             </div>
 
                                             <div class="form-button-row d-flex justify-content-center flex-row mt-3">
-                                                <button class="btn btn-credits shadow-sm my-2" type="submit" name="submit">Inloggen</button>
+                                                <button class="btn btn-credits shadow-sm my-2" type="submit" name="submit"><?= $translator->__('Inloggen') ?></button>
                                             </div>
 
                                             <div class="forgotpassword mt-5 mb-3 d-flex justify-content-center">
-                                                <a href="../view/forgotPassword.php">Wachtwoord vergeten?</a>
+                                                <a href="../view/forgotPassword.php"><?= $translator->__('Wachtwoord vergeten?') ?></a>
                                             </div>
                                             <?php
                                             if (isset($_GET["error"])) {
@@ -125,4 +146,5 @@ class login extends View
     }
 }
 new login();
-include_once '../footer.php';
+// Include the footer
+$footer = new footer();
