a8b006de07d59d1d9a170a37670ef92ee9384b3b
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-29 21:11:41 +0200

    added translation

--- view/contact.php
+++ view/contact.php
@@ -3,39 +3,60 @@
 // Define the namespace of this class
 namespace View;
 
-// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
-require_once '../vendor/autoload.php';
+/* Echo errors for development purposes */
 
-// Import classes this class depends on
-use Framework\View;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// If a session variable array exists store the contents in the form_data variable
-// So we can retain the form values for better user experience
-if (isset($_SESSION['contact_form']) && !empty($_SESSION['contact_form'])) {
-	$form_data = $_SESSION['contact_form'];
-	unset($_SESSION['contact_form']);
+if (session_status() === PHP_SESSION_NONE) {
+	session_start();
 }
 
 // Setting the ROOT directory for this file so the relative paths used in included pages will still work
 $ROOT = '../';
 
-// Include the header
-include_once '../header.php';
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require_once $ROOT . 'vendor/autoload.php';
+
+// Import classes this class depends on
+use Framework\View;
+use View\header;
+use Controller\translatorController;
+
+// Used to translate the header on this page
+$translator = new translatorController;
+// Use the getLanguageFile method of the languageSelector and require the correct language file
+require $ROOT . $translator->getLanguageFile();
 
 // Contact class that contains the contact form
-
 class contact extends View
 {
 
 	public function show()
 	{
+		$ROOT = '../';
+
+		// Used to translate page content
+		$translator = new translatorController;
+		// Use the getLanguageFile method of the languageSelector and require the correct language file
+		require $ROOT . $translator->getLanguageFile();
+
+		$header = new header();
+
+		// If a session variable array exists store the contents in the form_data variable
+		// So we can retain the form values for better user experience
+		if (isset($_SESSION['contact_form']) && !empty($_SESSION['contact_form'])) {
+			$form_data = $_SESSION['contact_form'];
+			unset($_SESSION['contact_form']);
+		}
 
 ?>
 
 		<section>
 			<div class="container">
 				<div class=" text-center mt-2 ">
-					<h1>Contactformulier</h1>
+					<h1><?= $translator->__('Contactformulier') ?></h1>
 				</div>
 
 				<div class="row mt-3">
@@ -49,23 +70,23 @@ class contact extends View
 										<div class="row">
 											<div class="col-md-6">
 												<div class="form-group">
-													<label for="form_name">Voornaam:</label>
-													<input id="form_name" type="text" name="name" class="form-control border-0" placeholder="Voer uw voornaam in" value="<?php if (isset($form_data)) {
-																																												echo $form_data['name'];
-																																											} ?>" required>
+													<label for="form_name"><?= $translator->__('Voornaam:') ?></label>
+													<input id="form_name" type="text" name="name" class="form-control border-0" placeholder="<?= $translator->__('Voer uw voornaam in') ?>" value="<?php if (isset($form_data)) {
+																																																		echo $form_data['name'];
+																																																	} ?>" required>
 													<div class="invalid-feedback">
-														Dit veld is verplicht.
+														<?= $translator->__('Dit veld is verplicht') ?>
 													</div>
 												</div>
 											</div>
 											<div class="col-md-6">
 												<div class="form-group">
-													<label for="form_lastname">Achternaam:</label>
-													<input id="form_lastname" type="text" name="lastname" class="form-control border-0" placeholder="Voer uw achternaam in" value="<?php if (isset($form_data)) {
-																																														echo $form_data['lastname'];
-																																													} ?>" required>
+													<label for="form_lastname"><?= $translator->__('Achternaam') ?>:</label>
+													<input id="form_lastname" type="text" name="lastname" class="form-control border-0" placeholder="<?= $translator->__('Voer uw achternaam in') ?>" value="<?php if (isset($form_data)) {
+																																																					echo $form_data['lastname'];
+																																																				} ?>" required>
 													<div class="invalid-feedback">
-														Dit veld is verplicht.
+														<?= $translator->__('Dit veld is verplicht') ?>
 													</div>
 												</div>
 											</div>
@@ -74,27 +95,27 @@ class contact extends View
 										<div class="row mt-3">
 											<div class="col-md-6">
 												<div class="form-group">
-													<label for="form_email">E-mailadres:</label>
-													<input id="form_email" type="email" name="email" class="form-control border-0" placeholder="Voer uw e-mailadres in" value="<?php if (isset($form_data)) {
-																																													echo $form_data['email'];
-																																												} ?>" required>
+													<label for="form_email"><?= $translator->__('E-mailadres') ?></label>
+													<input id="form_email" type="email" name="email" class="form-control border-0" placeholder="<?= $translator->__('Voer uw e-mailadres in') ?>" value="<?php if (isset($form_data)) {
+																																																				echo $form_data['email'];
+																																																			} ?>" required>
 													<div class="invalid-feedback">
-														Voer een geldig email-adress in.
+														<?= $translator->__('Voer een geldig e-mailadres in') ?>
 													</div>
 												</div>
 											</div>
 											<div class="col-md-6">
 												<div class="form-group">
-													<label for="form_need">Onderwerp:</label>
+													<label for="form_need"><?= $translator->__('Onderwerp:') ?></label>
 													<select id="form_need" name="need" class="form-control border-0" required>
-														<option value="" selected disabled>-- Kies een onderwerp --</option>
-														<option value="Suggestie / Feedback" <?php echo (isset($form_data) && $form_data['need'] == 'Suggestie / Feedback') ? 'selected' : ''; ?>>Suggestie / Feedback</option>
-														<option value="Klacht" <?php echo (isset($form_data) && $form_data['need'] == 'Klacht') ? 'selected' : ''; ?>>Klacht</option>
-														<option value="Vraag over de applicatie" <?php echo (isset($form_data) && $form_data['need'] == 'Vraag over de applicatie') ? 'selected' : ''; ?>>Vraag over de applicatie</option>
-														<option value="Overig" <?php echo (isset($form_data) && $form_data['need'] == 'Overif') ? 'selected' : ''; ?>>Overig</option>
+														<option value="" selected disabled>-- <?= $translator->__('Kies een onderwerp') ?> --</option>
+														<option value="Suggestie / Feedback" <?php echo (isset($form_data) && $form_data['need'] == 'Suggestie / Feedback') ? 'selected' : ''; ?>><?= $translator->__('Suggestie / Feedback') ?></option>
+														<option value="Klacht" <?php echo (isset($form_data) && $form_data['need'] == 'Klacht') ? 'selected' : ''; ?>><?= $translator->__('Klacht') ?></option>
+														<option value="Vraag over de applicatie" <?php echo (isset($form_data) && $form_data['need'] == 'Vraag over de applicatie') ? 'selected' : ''; ?>><?= $translator->__('Vraag over de applicatie') ?></option>
+														<option value="Overig" <?php echo (isset($form_data) && $form_data['need'] == 'Overig') ? 'selected' : ''; ?>><?= $translator->__('Overig') ?></option>
 													</select>
 													<div class="invalid-feedback">
-														Kies een onderwerp.
+														<?= $translator->__('Kies een onderwerp') ?>
 													</div>
 												</div>
 											</div>
@@ -103,12 +124,12 @@ class contact extends View
 										<div class="row mt-3">
 											<div class="col-md-12">
 												<div class="form-group">
-													<label for="form_message">Bericht:</label>
-													<textarea id="form_message" name="message" class="form-control border-0" placeholder="Schrijf hier uw bericht. (20 -500 karakters)" rows="4" required><?php if (isset($form_data)) {
-																																																				echo $form_data['message'];
-																																																			} ?></textarea>
+													<label for="form_message"><?= $translator->__('Bericht:') ?></label>
+													<textarea id="form_message" name="message" class="form-control border-0" placeholder="<?= $translator->__('Schrijf hier uw bericht. (20 - 500 karakters)') ?>" rows="4" required><?php if (isset($form_data)) {
+																																																											echo $form_data['message'];
+																																																										} ?></textarea>
 													<div class="invalid-feedback">
-														Dit veld is verplicht. (20 - 500 karakters)
+														<?= $translator->__('Dit veld is verplicht. (20 - 500 karakters)') ?>
 													</div>
 												</div>
 											</div>
@@ -116,16 +137,16 @@ class contact extends View
 											<div class="form-check form-switch mt-4">
 												<input class="form-check-input" type="checkbox" role="switch" name="privacy-policy" id="privacy-policy" value="agreed" required>
 												<label class="form-check-label" for="privacy-policy">
-													Ik ga akkoord met het <a href="./privacy-policy.php" target=”_blank”>privacy
-														beleid</a>.
+													<?= $translator->__('Ik ga akkoord met het ') ?> <a href="./privacyPolicy.php" target=”_blank”><?= $translator->__('privacy beleid.') ?></a>
+
 												</label>
 												<div class="invalid-feedback">
-													U dient akkoord te gaan met ons privacy beleid.
+													<?= $translator->__('U dient akkoord te gaan met ons privacy beleid.') ?>
 												</div>
 											</div>
 
 											<div class="form-button-row d-flex flex-row mt-3">
-												<button class="btn btn-credits shadow-sm my-2" name="submit" type="submit">Verzenden</button>
+												<button class="btn btn-credits shadow-sm my-2" name="submit" type="submit"><?= $translator->__('Verzenden') ?></button>
 											</div>
 											<?php
 											if (isset($_GET["error"])) {
@@ -156,4 +177,5 @@ class contact extends View
 	}
 }
 new contact();
-include_once '../footer.php';
+// Include the footer
+$footer = new footer();
