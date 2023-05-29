b720baee819c61c4560ecd4420ed65be0b2b3d42
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-05-29 21:10:42 +0200

    created language file

---
+++ assets/languages/en.php
@@ -0,0 +1,138 @@
+<?php
+
+$lang = [
+    // Navbar
+    "Alles voor je dier op de wandelkaart" => "Everything for your pet on our map",
+    "Taal" => "Language",
+    "Over ons" => "About us",
+    "App features" => "App features",
+    "Impressie" => "Impression",
+    "Abbonementen" => "Payment plans",
+    "Betatesters" => "Betatesters",
+    "Uitloggen" => "Log out",
+    "Admin Dashboard" => "Admin Dashboard",
+    "Account" => "Account",
+    "Gebruikersprofiel" => "Userprofile",
+    "Wijzig profiel" => "Edit userprofile",
+    "Vind matches" => "Find matches",
+    "Creëer gebruikersprofiel" => "Create userprofile",
+    "Registreren" => "Register",
+    "Log in" => "Sign in",
+
+    // Banner
+    "Binnenkort beschikbaar voor iOS en Android" => "Available for iOS and Android soon!",
+    "De meet-up app voor" => "The meet-up app for",
+    "jou en je viervoeter" => "you and your pet",
+    "Playm8 is de app waarbij je connecties aangaat samen met je huisdier. Swipe, like, chat en ontmoet baasjes en hun huisdieren bij jou in je buurt. Op zoek naar de dichtstbijzijnde dierenvoorziening? Een last minute oppas? Een beschikbare uitlaatservice? Playm8 heeft het." =>
+    "Playm8 is the app where you make connections together with your pet. Swipe, like, chat, and meet owners and their pets in your area. Looking for the nearest pet facility? Last-minute pet sitting? An available dog walking service? Playm8 has it.",
+
+    // About us
+    "Data tegen eenzaamheid" => "Data against loneliness",
+    "Eenzaamheid is een groot maatschappelijk probleem in Nederland; in een onderzoek van het RIVM uit 2020 gaf maar liefst 47% van de volwassen bevolking (18 jaar en ouder) aan eenzaam te zijn. De cijfers van 85-plussers zijn nog schrikbarender: 66% gaf hier aan zich regelmatig eenzaam te voelen (Eenzaamheid | Infographic, z.d.)."
+    => "Loneliness is a major societal problem in the Netherlands; in a 2020 study by the dutch RIVM, a staggering 47% of the adult population (18 years and older) reported feeling lonely. The figures for those aged 85 and older are even more alarming: 66% indicated that they regularly experience loneliness (Loneliness | Infographic, n.d.).",
+    "De visie van Playm8 is om eenzaamheid in onze maatschappij met behulp van technologie en data te reduceren. Onze organisatie hecht waarde aan vriendschap en verbinding in het leven. Het is altijd fijn om een persoon in je leven te hebben die jou kan laten lachen zelfs als diegene niet bij je is." => "Playm8's vision is to reduce loneliness in our society through the use of technology and data. Our organization values friendship and connection in life. It's always great to have someone in your life who can make you laugh, even when they're not physically present.",
+    "Het leven draait niet om wat je hebt, maar om wie je hebt, dat is wat telt." => "Life is not about what you have, but about who you have. That's what matters.",
+
+    // Features section
+    "Features" => "Features",
+    "Krijg snel inzichtelijk waar de dichtstbijzijnde hondentrimmer of hondenuitlaatplaats te vinden is." => "Quickly find out where to locate the nearest dog groomer or dog park.",
+    "In-app oppas en uitlaatservice" => "In-app pet sitting and dog walking service.",
+    "Op zoek naar een uitlaatservice? Of een service aanbieden? Kom in contact met elkaar." => "Looking for a dog walking service? Or offering a service? Connect with each other.",
+    "Een (gezond) maatje voor het leven" => "A (healthy) buddy for life",
+    "Dieren die bewegen blijven fit en gezond. Voldoende beweging houdt de spieren en botten sterk, zorgt ervoor dat het hart en de longen gezond blijven en voorkomt overgewicht." => "Animals that move stay fit and healthy. Sufficient exercise keeps muscles and bones strong, helps maintain a healthy heart and lungs, and prevents overweight.",
+    "Krijg snel inzichtelijk waar de dichtstbijzijnde hondentrimmer of dierenarts te vinden is." => "Quickly find out where to locate the nearest dog groomer or veterinarian.",
+    "Chat en maak nieuwe vrienden" => "Chat and make new friends",
+    "Chat met huisdiereigenaren in de buurt en maak nieuwe vrienden." => "Chat with pet owners in your neighborhood and make new friends.",
+    "Vind de dichtsbijzijnde dierenarts" => "Find the closest veterinarian",
+    "Vind en kom gemakkelijk in contact met verschillende dierenartspraktijken in uw buurt." => "Easily find and get in touch with various veterinary clinics in your area.",
+    "Impressie" => "Impression",
+
+    // Credit section
+    "Credits" => "Credits",
+    "Pakket 1" => "Package 1",
+    "Chihuahua" => "Chihuahua",
+    "Maximaal 50 swipes per 24 uur" => "Maximum of 50 swipes per 24 hours.",
+    "Binnenkort verkrijgbaar" => "Available soon",
+    "Pakket 2" => "Package 2",
+    "Labrador" => "Labrador",
+    "Maximaal 150 swipes per 24 uur" => "Maximum of 150 swipes per 24 hours.",
+    "Pakket 3" => "Package 3",
+    "Pitbull" => "Pitbull",
+    "Onbeperkt aantal swipes" => "Unlimited swipes.",
+    "Reclamevrij" => "No advertisements",
+
+    // Beta form
+    "Registreer je nu!" => "Register now!",
+    "Gebruikersnaam" => "Username",
+    "Dit veld is verplicht" => "This field is required",
+    "Voer een geldig e-mailadres in." => "Enter a valid email adress",
+    "Ik ga akkoord met het " => "I agree with the ",
+    "privacy beleid." => "privacy policy.",
+    "U dient akkoord te gaan met ons privacy beleid." => "You are required to agree with our privacy policy.",
+    "Ik ga akkoord met de " => "I agree with the ",
+    "algemene voorwaarden." => "terms and conditions",
+    "U dient akkoord te gaan met onze algemene voorwaarden." => "You are required to agree with our terms and conditions.",
+    "Bedankt voor uw registratie." => "Thank your for registering",
+    "Verzenden" => "Submit",
+    "Alle velden zijn verplicht." => "All fields are required.",
+    "Onjuist e-mail format." => "Incorrect e-mail format",
+    "Dit e-mailadres is niet bij ons bekend." => "Unknown email.",
+    "Dit account staat al geregistreerd als beta-tester." => "Account already registered as a beta-tester.",
+    "Dit account is inactief." => "This account is inactive",
+    "Bedankt voor je inschrijving als betatester." => "Thank your for registering as a betatester",
+    "Wij behandelen je verzoek zo snel mogelijk." => "We will process your request as soon as possible.",
+    "Meld je aan en wordt Betatester!" => "Apply to become a betatester!",
+    "Bij Playm8 zijn wij op zoek naar mensen met voldoende analytisch denkvermogen en affiniteit met IT die Beta-tester willen worden. Met ons Beta-test programma willen wij de functionaliteit en de bruikbaarheid van onze mobiele applicatie testen. Issues en bugs worden direct met onze software-ontwikkelaars teruggekoppeld wat bijdraagt aan de kwaliteit van ons product." => "At Playm8, we are looking for individuals with sufficient analytical thinking skills and an affinity for IT who want to become beta testers. With our Beta Test Program, we aim to test the functionality and usability of our mobile application. Issues and bugs are promptly reported to our software developers, contributing to the quality of our product.",
+    "Meld je aan met de gebruikersnaam en het e-mailadres van je Playm8 account!" => "Apply with the username and email of your Playm8 account!",
+
+    // Footer
+    "Heeft u vragen, problemen of feedback voor ons?" => "Questions, problems or feedback?",
+    "Contacteer ons via onze socials of gebruik ons " => "Contact us trough our socials or use our ",
+    "contact-formulier." => "contact form",
+
+    // Signup page
+    "Gebruikersnaam" => "Username",
+    "E-mailadres" => "Email",
+    "Wachtwoord" => "Password",
+    "Herhaal uw wachtwoord" => "Repeat your password",
+
+    // Login page
+    "Inloggen" => "Log in",
+    "Wachtwoord vergeten?" => "Forgot password?",
+
+    // Contact form
+    "Contactformulier" => "Contact form",
+    "Voornaam:" => "First name:",
+    "Voer uw voornaam in" => "Enter your first name",
+    "Achternaam:" => "Last name:",
+    "Voer uw achternaam in" => "Enter your last name",
+    "Voer uw e-mailadres in" => "Enter your email",
+    "Voer een geldig e-mailadres in" => "Enter a valid email",
+    "Onderwerp" => "Subject",
+    "Kies een onderwerp" => "Choose subject",
+    "Suggestie / Feedback" => "Suggestion / Feedback",
+    "Klacht" => "Complaint",
+    "Vraag over de applicatie" => "Question about the app",
+    "Overig" => "Other",
+    "Kies een onderwerp" => "Select a subject",
+    "Bericht:" => "Message:",
+    "Schrijf hier uw bericht. (20 - 500 karakters)" => "Write your message here (20 - 500 characters)",
+    "Dit veld is verplicht. (20 - 500 karakters)" => "This field is required (20 - 500 characters)",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
