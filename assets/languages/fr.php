278d481939ad021e69d53d7100a7c100561d91af
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-06-04 21:07:42 +0200

    fixed translation

--- assets/languages/fr.php
+++ assets/languages/fr.php
@@ -1,16 +1,168 @@
 <?php
 
 $lang = [
-    "Home" => "Maison",
-    "About us" => "À propos de nous",
-    "Contact us" => "À propos de nous",
-    "Login" => "Connexion",
-    "Signup" => "Connexion",
-    "Hi" => "Salut",
-    "Language" => "Langue",
-    "Profile" => "Profil",
-    "Settings" => "Paramètres",
-    "Logout" => "Se déconnecter",
+    // Navbar
+    "Alles voor je dier op de wandelkaart" => "Tout pour votre animal sur notre carte",
+    "Taal" => "Langue",
+    "Over ons" => "À propos de nous",
+    "App Features" => "Fonctionnalités de l'application",
+    "Impressie" => "Impression",
+    "Abbonementen" => "Abonnements",
+    "Betatesters" => "Bêta-testeurs",
+    "Uitloggen" => "Déconnexion",
+    "Admin Dashboard" => "Tableau de bord administrateur",
+    "Account" => "Compte",
+    "Gebruikersprofiel" => "Profil utilisateur",
+    "Wijzig profiel" => "Modifier le profil",
+    "Vind matches" => "Trouver des correspondances",
+    "Creëer gebruikersprofiel" => "Créer un profil utilisateur",
+    "Registreren" => "S'inscrire",
+    "Inloggen" => "Se connecter",
 
+    // Banner
+    "Binnenkort beschikbaar voor iOS en Android!" => "Bientôt disponible sur iOS et Android !",
+    "De meet-up app voor" => "L'application de rencontre pour",
+    "jou en je viervoeter" => "toi et ton animal de compagnie",
+    "Playm8 is de app waarbij je connecties aangaat samen met je huisdier. Swipe, like, chat en ontmoet baasjes en hun huisdieren bij jou in je buurt. Op zoek naar de dichtstbijzijnde dierenvoorziening? Een last minute oppas? Een beschikbare uitlaatservice? Playm8 heeft het." =>
+    "Playm8 est l'application qui te permet de faire des rencontres avec ton animal de compagnie. Fais défiler, aime, discute et rencontre des propriétaires et leurs animaux de compagnie près de chez toi. À la recherche du service pour animaux de compagnie le plus proche ? Besoin d'une garde de dernière minute ? D'un service de promenade disponible ? Playm8 l'a.",
+
+    // About us
+    "Data tegen eenzaamheid" => "Données contre la solitude",
+    "Eenzaamheid is een groot maatschappelijk probleem in Nederland; in een onderzoek van het RIVM uit 2020 gaf maar liefst 47% van de volwassen bevolking (18 jaar en ouder) aan eenzaam te zijn. De cijfers van 85-plussers zijn nog schrikbarender: 66% gaf hier aan zich regelmatig eenzaam te voelen (Eenzaamheid | Infographic, z.d.)."
+    => "La solitude est un problème majeur dans la société néerlandaise ; selon une étude de l'Institut national de la santé publique et de l'environnement réalisée en 2020, pas moins de 47 % de la population adulte (18 ans et plus) a déclaré se sentir seul. Les chiffres pour les personnes de 85 ans et plus sont encore plus alarmants : 66 % d'entre elles ont indiqué ressentir régulièrement de la solitude (Solitude | Infographie, s.d.).",
+    "De visie van Playm8 is om eenzaamheid in onze maatschappij met behulp van technologie en data te reduceren. Onze organisatie hecht waarde aan vriendschap en verbinding in het leven. Het is altijd fijn om een persoon in je leven te hebben die jou kan laten lachen zelfs als diegene niet bij je is." => "La vision de Playm8 est de réduire la solitude dans notre société grâce à l'utilisation de la technologie et des données. Notre organisation valorise l'amitié et la connexion dans la vie. Il est toujours agréable d'avoir quelqu'un dans sa vie qui peut vous faire rire, même s'il n'est pas physiquement présent.",
+    "Het leven draait niet om wat je hebt, maar om wie je hebt, dat is wat telt." => "La vie ne tourne pas autour de ce que vous avez, mais de qui vous avez. C'est ce qui compte.",
+
+    // Features section
+    "Features" => "Fonctionnalités",
+    "Krijg snel inzichtelijk waar de dichtstbijzijnde hondentrimmer of hondenuitlaatplaats te vinden is." => "Obtenez rapidement des informations sur l'emplacement du toiletteur pour chiens ou du parc pour chiens le plus proche.",
+    "In-app oppas en uitlaatservice" => "Service de garde et de promenade pour animaux intégré à l'application.",
+    "Op zoek naar een uitlaatservice? Of een service aanbieden? Kom in contact met elkaar." => "Vous recherchez un service de promenade pour chiens ? Ou vous souhaitez proposer un service ? Entrez en contact les uns avec les autres.",
+    "Een (gezond) maatje voor het leven" => "Un compagnon (sain) pour la vie",
+    "Dieren die bewegen blijven fit en gezond. Voldoende beweging houdt de spieren en botten sterk, zorgt ervoor dat het hart en de longen gezond blijven en voorkomt overgewicht." => "Les animaux qui bougent restent en forme et en bonne santé. L'exercice régulier maintient les muscles et les os solides, favorise un cœur et des poumons sains et prévient l'excès de poids.",
+    "Krijg snel inzichtelijk waar de dichtstbijzijnde hondentrimmer of dierenarts te vinden is." => "Obtenez rapidement des informations sur l'emplacement du toiletteur pour chiens ou du vétérinaire le plus proche.",
+    "Chat en maak nieuwe vrienden" => "Discutez et faites de nouveaux amis",
+    "Chat met huisdiereigenaren in de buurt en maak nieuwe vrienden." => "Discutez avec les propriétaires d'animaux de compagnie de votre quartier et faites de nouveaux amis.",
+    "Vind de dichtsbijzijnde dierenarts" => "Trouvez le vétérinaire le plus proche",
+    "Vind en kom gemakkelijk in contact met verschillende dierenartspraktijken in uw buurt." => "Trouvez facilement et entrez en contact avec différentes cliniques vétérinaires de votre région.",
+    "Impressie" => "Impression",
+
+    // Credit section
+    "Credits" => "Crédits",
+    "Pakket 1" => "Forfait 1",
+    "Chihuahua" => "Chihuahua",
+    "Maximaal 50 swipes per 24 uur" => "Maximum de 50 balayages par 24 heures",
+    "Binnenkort verkrijgbaar" => "Bientôt disponible",
+    "Pakket 2" => "Forfait 2",
+    "Labrador" => "Labrador",
+    "Maximaal 150 swipes per 24 uur" => "Maximum de 150 balayages par 24 heures",
+    "Pakket 3" => "Forfait 3",
+    "Pitbull" => "Pitbull",
+    "Onbeperkt aantal swipes" => "Nombre illimité de balayages",
+    "Reclamevrij" => "Sans publicité",
+
+    // Beta form
+    "Registreer je nu!" => "Inscrivez-vous maintenant !",
+    "Gebruikersnaam" => "Nom d'utilisateur",
+    "E-mailadres" => "Adresse e-mail",
+    "Dit veld is verplicht" => "Ce champ est obligatoire",
+    "Voer een geldig e-mailadres in." => "Entrez une adresse e-mail valide.",
+    "Ik ga akkoord met het " => "J'accepte les ",
+    "privacy beleid." => "politique de confidentialité.",
+    "U dient akkoord te gaan met ons privacy beleid." => "Vous devez accepter notre politique de confidentialité.",
+    "Ik ga akkoord met de " => "J'accepte les ",
+    "algemene voorwaarden." => "conditions générales.",
+    "U dient akkoord te gaan met onze algemene voorwaarden." => "Vous devez accepter nos conditions générales.",
+    "Bedankt voor uw registratie." => "Merci de vous être inscrit.",
+    "Verzenden" => "Envoyer",
+    "Alle velden zijn verplicht." => "Tous les champs sont obligatoires.",
+    "Onjuist e-mail format." => "Format d'e-mail incorrect.",
+    "Dit e-mailadres is niet bij ons bekend." => "Cette adresse e-mail n'est pas reconnue.",
+    "Dit account staat al geregistreerd als beta-tester." => "Ce compte est déjà enregistré en tant que bêta-testeur.",
+    "Dit account is inactief." => "Ce compte est inactif.",
+    "Bedankt voor je inschrijving als betatester." => "Merci de vous être inscrit en tant que bêta-testeur.",
+    "Wij behandelen je verzoek zo snel mogelijk." => "Nous traiterons votre demande dans les plus brefs délais.",
+    "Meld je aan en wordt Betatester!" => "Inscrivez-vous et devenez bêta-testeur !",
+    "Bij Playm8 zijn wij op zoek naar mensen met voldoende analytisch denkvermogen en affiniteit met IT die Beta-tester willen worden. Met ons Beta-test programma willen wij de functionaliteit en de bruikbaarheid van onze mobiele applicatie testen. Issues en bugs worden direct met onze software-ontwikkelaars teruggekoppeld wat bijdraagt aan de kwaliteit van ons product." => "Chez Playm8, nous recherchons des personnes ayant des compétences analytiques suffisantes et une affinité pour l'informatique qui souhaitent devenir des testeurs bêta. Avec notre programme de test bêta, nous visons à tester la fonctionnalité et l'utilisabilité de notre application mobile. Les problèmes et les bugs sont signalés directement à nos développeurs de logiciels, ce qui contribue à la qualité de notre produit.",
+    "Meld je aan met de gebruikersnaam en het e-mailadres van je Playm8 account!" => "Inscrivez-vous avec le nom d'utilisateur et l'adresse e-mail de votre compte Playm8 !",
+
+    // Footer
+    "Heeft u vragen, problemen of feedback voor ons?" => "Des questions, des problèmes ou des commentaires pour nous ?",
+    "Contacteer ons via onze socials of gebruik ons " => "Contactez-nous via nos réseaux sociaux ou utilisez notre ",
+    "contactformulier." => "formulaire de contact.",
+    "Afbeelding via" => "Image via",
+    "op" => "sur",
+
+    // Signup page
+    "Gebruikersnaam:" => "Nom d'utilisateur :",
+    "E-mailadres:" => "Adresse e-mail :",
+    "Wachtwoord:" => "Mot de passe :",
+    "Herhaal uw wachtwoord:" => "Répétez votre mot de passe :",
+
+    // Login page
+    "Wachtwoord vergeten?" => "Mot de passe oublié ?",
+
+    // Forgot password page
+    "Wachtwoord vergeten?" => "Mot de passe oublié ?",
+    "Je kunt je wachtwoord opnieuw instellen, voer hiervoor je e-mailadres in en volg de daarna de instructies die je via de e-mail krijgt toegestuurd." => "Entrez votre adresse e-mail et suivez les instructions envoyées par e-mail pour réinitialiser votre mot de passe.",
+    "Verstuur verzoek" => "Envoyer la demande",
+
+    // Contact form
+    "Contactformulier" => "Formulaire de contact",
+    "Voornaam:" => "Prénom :",
+    "Voer uw voornaam in" => "Entrez votre prénom",
+    "Achternaam:" => "Nom de famille:",
+    "Voer uw achternaam in" => "Entrez votre nom de famille",
+    "Voer uw e-mailadres in" => "Entrez votre adresse e-mail",
+    "Voer een geldig e-mailadres in" => "Entrez une adresse e-mail valide",
+    "Onderwerp:" => "Sujet:",
+    "Kies een onderwerp" => "Choisissez un sujet",
+    "Suggestie / Feedback" => "Suggestion / Commentaire",
+    "Klacht" => "Plainte",
+    "Vraag over de applicatie" => "Question sur l'application",
+    "Overig" => "Autre",
+    "Kies een onderwerp" => "Sélectionnez un sujet",
+    "Bericht:" => "Message :",
+    "Schrijf hier uw bericht. (20 - 500 karakters)" => "Rédigez votre message ici (20 - 500 caractères)",
+    "Dit veld is verplicht. (20 - 500 karakters)" => "Ce champ est obligatoire (20 - 500 caractères)",
+
+    // Account page
+    "Wijzig account" => "Modifier le compte",
+    "Wijzig hier je gebruikersnaam, email en wachtwoord." => "Modifiez votre nom d'utilisateur, votre adresse e-mail et votre mot de passe ici.",
+    "Nieuw wachtwoord:" => "Nouveau mot de passe :",
+    "Herhaal nieuw wachtwoord:" => "Répétez le nouveau mot de passe :",
+    "Huidig wachtwoord:" => "Mot de passe actuel :",
+    "Voer nieuw wachtwoord in" => "Entrez le nouveau mot de passe",
+    "Voer huidig wachtwoord in" => "Entrez le mot de passe actuel",
+    "Annuleer" => "Annuler",
+    "Opslaan" => "Enregistrer",
+    "Verwijder account" => "Supprimer le compte",
+    "Bevestig verwijderen van account" => "Confirmez la suppression du compte",
+    "Voer de 5 cijfers in ter bevestiging" => "Entrez les 5 chiffres pour confirmer",
+    "Bevestig" => "Confirmer",
+
+    // Userprofile Page
+    "Wijzig profiel" => "Modifier le profil",
+    "Recente afbeeldingen" => "Images récentes",
+    "Alle afbeeldingen weergeven" => "Afficher toutes les images",
+
+    // Edit userprofile page
+    "Wijzig gebruikersprofiel" => "Modifier le profil d'utilisateur",
+    "Wijzig hier de gegevens van het gebruikersprofiel dat aan andere Playm8 gebruikers wordt getoond." => "Modifiez les informations du profil utilisateur affichées aux autres utilisateurs de Playm8.",
+    "Wijzig profielfoto" => "Modifier la photo de profil",
+    "Stad:" => "Ville :",
+    "Voer uw stad in" => "Entrez votre ville",
+    "Land:" => "Pays :",
+    "Voer uw land in" => "Entrez votre pays",
+    "Telefoonnummer:" => "Numéro de téléphone :",
+    "Voer uw telefoonnummer in" => "Entrez votre numéro de téléphone",
+    "Geboortedatum:" => "Date de naissance :",
+    "Over mij koptekst:" => "En-tête à mon sujet :",
+    "Voer de titel van uw profielpagina in" => "Entrez le titre de votre page de profil",
+    "Over mij:" => "À propos de moi :",
+    "Vertel hier over de favoriete activiteiten, uitlaatplaatsen of andere leuke feitjes over jou en je huisdier! (20 - 255 karakters)" => "Parlez de vos activités préférées, des lieux de promenade ou d'autres anecdotes amusantes vous concernant, vous et votre animal de compagnie ! (20 - 255 caractères)",
+    "Dit veld is verplicht. (20 - 500 karakters)" => "Ce champ est obligatoire. (20 - 500 caractères)",
+    "Verwijder gebruikersprofiel" => "Supprimer le profil d'utilisateur",
+    "Bevestig verwijderen van gebruikersprofiel" => "Confirmer la suppression du profil utilisateur"
 
 ];
