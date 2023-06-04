b8bbb642ae4fe7af95c7a4e2051479441cd1f391
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-06-04 21:08:14 +0200

    added translation

--- view/forgotPassword.php
+++ view/forgotPassword.php
@@ -7,18 +7,23 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+// Setting the ROOT directory for this file so the relative paths used in included pages will still work
+$ROOT = '../';
+
 // Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
-require_once '../vendor/autoload.php';
+require_once $ROOT . 'vendor/autoload.php';
 
 // Import classes this class depends on
 use Framework\View;
+use View\header;
 use Controller\translatorController;
 
-// Setting the ROOT directory for this file so the relative paths used in any included pages will still work
-$ROOT = '../';
+// Used to translate the header on this page
+$translator = new translatorController;
+// Use the getLanguageFile method of the languageSelector and require the correct language file
+require $ROOT . $translator->getLanguageFile();
 
 // forgotPassword class that has a form that allows the user to reset his password
-
 class forgotPassword extends View
 {
 
@@ -44,24 +49,24 @@ class forgotPassword extends View
                                     <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
                                         <div class="mx-4 mb-5">
-                                            <h3 class="text-center mb-2">Wachtwoord vergeten?</h3>
-                                            <p>Je kunt je wachtwoord opnieuw instellen, voer hiervoor je e-mailadres in en volg de daarna de instructies die je via de e-mail krijgt toegestuurd.</p>
+                                            <h3 class="text-center mb-2"><?= $translator->__('Wachtwoord vergeten?') ?></h3>
+                                            <p><?= $translator->__('Je kunt je wachtwoord opnieuw instellen, voer hiervoor je e-mailadres in en volg de daarna de instructies die je via de e-mail krijgt toegestuurd.') ?></p>
                                         </div>
 
                                         <form action="../includes/forgotpassword.inc.php" method="post" class="needs-validation mx-1 mx-md-4" novalidate>
                                             <div class="d-flex flex-row align-items-center mb-4">
                                                 <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                                 <div class="form-outline flex-fill mb-0">
+                                                    <label for="form_email" class="form-label"><?= $translator->__('E-mailadres:') ?></label>
                                                     <input id="form_email" type="email" name="email" class="form-control" required>
-                                                    <label for="form_email" class="form-label">E-mailadres</label>
                                                     <div class="invalid-feedback">
-                                                        Dit veld is verplicht.
+                                                        <?= $translator->__('Dit veld is verplicht.') ?>
                                                     </div>
                                                 </div>
                                             </div>
 
                                             <div class="form-button-row d-flex justify-content-center flex-row mt-3">
-                                                <button class="btn btn-credits shadow-sm my-2" type="submit" name="submit">Verstuur</button>
+                                                <button class="btn btn-credits shadow-sm my-2" type="submit" name="submit"><?= $translator->__('Verstuur verzoek') ?></button>
                                             </div>
                                             <?php
                                             if (isset($_GET["error"])) {
