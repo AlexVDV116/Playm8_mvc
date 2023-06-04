e145515f9bb90d2cc8b95e5fca2a95f2835b4fef
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-06-04 21:07:46 +0200

    fixed translation

--- assets/languages/zh.php
+++ assets/languages/zh.php
@@ -1,16 +1,169 @@
 <?php
 
 $lang = [
-    "Home" => "家",
-    "About us" => "關於我們",
-    "Contact us" => "聯繫我們",
-    "Login" => "登錄",
-    "Signup" => "登錄",
-    "Hi" => "你好",
-    "Language" => "語言",
-    "Profile" => "輪廓",
-    "Settings" => "設置",
-    "Logout" => "登出",
+    // Navbar
+    "Alles voor je dier op de wandelkaart" => "在我們的地圖上，關於你的寵物一切",
+    "Taal" => "語言",
+    "Over ons" => "關於我們",
+    "App Features" => "應用程式特色",
+    "Impressie" => "印象",
+    "Abbonementen" => "訂閱方案",
+    "Betatesters" => "測試使用者",
+    "Uitloggen" => "登出",
+    "Admin Dashboard" => "管理員儀表板",
+    "Account" => "帳號",
+    "Gebruikersprofiel" => "使用者個人檔案",
+    "Wijzig profiel" => "編輯使用者個人檔案",
+    "Vind matches" => "尋找匹配",
+    "Creëer gebruikersprofiel" => "建立使用者個人檔案",
+    "Registreren" => "註冊",
+    "Inloggen" => "登入",
 
+    // Banner
+    "Binnenkort beschikbaar voor iOS en Android!" => "即將推出 iOS 和 Android 版本！",
+    "De meet-up app voor" => "適用於",
+    "jou en je viervoeter" => "你和你的寵物的見面應用程式",
+    "Playm8 is de app waarbij je connecties aangaat samen met je huisdier. Swipe, like, chat en ontmoet baasjes en hun huisdieren bij jou in je buurt. Op zoek naar de dichtstbijzijnde dierenvoorziening? Een last minute oppas? Een beschikbare uitlaatservice? Playm8 heeft het." =>
+    "Playm8 是一款讓你和你的寵物一起建立連結的應用程式。左滑、點讚、聊天，並在你的附近遇見其他主人和他們的寵物。尋找最近的寵物設施？臨時寵物照顧？有空的遛狗服務？Playm8 通通都有。",
+
+    // About us
+    "Data tegen eenzaamheid" => "致力于对抗孤独",
+    "Eenzaamheid is een groot maatschappelijk probleem in Nederland; in een onderzoek van het RIVM uit 2020 gaf maar liefst 47% van de volwassen bevolking (18 jaar en ouder) aan eenzaam te zijn. De cijfers van 85-plussers zijn nog schrikbarender: 66% gaf hier aan zich regelmatig eenzaam te voelen (Eenzaamheid | Infographic, z.d.)."
+    => "孤独是荷兰面临的一大社会问题；根据2020年荷兰国家公共卫生与环境研究所的调查，高达47%的成年人口（18岁及以上）表示感到孤独。而85岁及以上年龄段的人数更是令人震惊：66%的人表示他们经常感到孤独（孤独 | 信息图表，无日期）。",
+    "De visie van Playm8 is om eenzaamheid in onze maatschappij met behulp van technologie en data te reduceren. Onze organisatie hecht waarde aan vriendschap en verbinding in het leven. Het is altijd fijn om een persoon in je leven te hebben die jou kan laten lachen zelfs als diegene niet bij je is." => "Playm8 的愿景是通过技术和数据减少我们社会中的孤独问题。我们的组织重视生活中的友谊和连接。即使在没有他们在身边的时候，拥有一个能让你笑的人总是令人愉快的。",
+    "Het leven draait niet om wat je hebt, maar om wie je hebt, dat is wat telt." => "生活不是关于你拥有什么，而是关于你拥有谁。这才是重要的。",
+
+    // Features section
+    "Features" => "特点",
+    "Krijg snel inzichtelijk waar de dichtstbijzijnde hondentrimmer of hondenuitlaatplaats te vinden is." => "快速找到最近的狗美容师或狗公园的位置。",
+    "In-app oppas en uitlaatservice" => "应用内宠物寄养和遛狗服务。",
+    "Op zoek naar een uitlaatservice? Of een service aanbieden? Kom in contact met elkaar." => "寻找遛狗服务？或提供服务？相互联系吧。",
+    "Een (gezond) maatje voor het leven" => "终身的（健康）伙伴",
+    "Dieren die bewegen blijven fit en gezond. Voldoende beweging houdt de spieren en botten sterk, zorgt ervoor dat het hart en de longen gezond blijven en voorkomt overgewicht." => "活动的动物保持健康和强壮。充足的运动可以保持肌肉和骨骼强壮，有助于保持心脏和肺部健康，并预防超重。",
+    "Krijg snel inzichtelijk waar de dichtstbijzijnde hondentrimmer of dierenarts te vinden is." => "快速找出最近的狗美容师或兽医的位置。",
+    "Chat en maak nieuwe vrienden" => "聊天并交新朋友",
+    "Chat met huisdiereigenaren in de buurt en maak nieuwe vrienden." => "与附近的宠物主人聊天并结交新朋友。",
+    "Vind de dichtsbijzijnde dierenarts" => "找到最近的兽医",
+    "Vind en kom gemakkelijk in contact met verschillende dierenartspraktijken in uw buurt." => "轻松找到并与附近的各种兽医诊所联系。",
+    "Impressie" => "印象",
+
+    // Credit section
+    "Credits" => "积分",
+    "Pakket 1" => "套餐1",
+    "Chihuahua" => "吉娃娃",
+    "Maximaal 50 swipes per 24 uur" => "每24小时最多50次滑动。",
+    "Binnenkort verkrijgbaar" => "即将推出",
+    "Pakket 2" => "套餐2",
+    "Labrador" => "拉布拉多",
+    "Maximaal 150 swipes per 24 uur" => "每24小时最多150次滑动。",
+    "Pakket 3" => "套餐3",
+    "Pitbull" => "比特犬",
+    "Onbeperkt aantal swipes" => "无限次滑动。",
+    "Reclamevrij" => "无广告",
+
+    // Beta form
+    "Registreer je nu!" => "立即注册！",
+    "Gebruikersnaam" => "用户名",
+    "E-mailadres" => "電子郵件地址",
+    "Dit veld is verplicht" => "此字段为必填项",
+    "Voer een geldig e-mailadres in." => "请输入有效的电子邮件地址",
+    "Ik ga akkoord met het " => "我同意",
+    "privacy beleid." => "隐私政策。",
+    "U dient akkoord te gaan met ons privacy beleid." => "您必须同意我们的隐私政策。",
+    "Ik ga akkoord met de " => "我同意",
+    "algemene voorwaarden." => "条款和条件",
+    "U dient akkoord te gaan met onze algemene voorwaarden." => "您必须同意我们的条款和条件。",
+    "Bedankt voor uw registratie." => "感谢您的注册",
+    "Verzenden" => "提交",
+    "Alle velden zijn verplicht." => "所有字段都是必填项。",
+    "Onjuist e-mail format." => "不正确的电子邮件格式",
+    "Dit e-mailadres is niet bij ons bekend." => "未知电子邮件。",
+    "Dit account staat al geregistreerd als beta-tester." => "该帐户已注册为Beta测试员。",
+    "Dit account is inactief." => "该帐户处于非活动状态",
+    "Bedankt voor je inschrijving als betatester." => "感谢您注册为Beta测试员",
+    "Wij behandelen je verzoek zo snel mogelijk." => "我们将尽快处理您的请求。",
+    "Meld je aan en wordt Betatester!" => "报名成为Beta测试员！",
+    "Bij Playm8 zijn wij op zoek naar mensen met voldoende analytisch denkvermogen en affiniteit met IT die Beta-tester willen worden. Met ons Beta-test programma willen wij de functionaliteit en de bruikbaarheid van onze mobiele applicatie testen. Issues en bugs worden direct met onze software-ontwikkelaars teruggekoppeld wat bijdraagt aan de kwaliteit van ons product." => "Playm8正在寻找具有足够分析思维能力和IT背景的人员加入Beta测试团队。通过我们的Beta测试计划，我们旨在测试我们的移动应用程序的功能和可用性。问题和错误将直接反馈给我们的软件开发人员，从而提高产品的质量。",
+    "Meld je aan met de gebruikersnaam en het e-mailadres van je Playm8 account!" => "使用您的Playm8账户的用户名和电子邮件地址进行报名！",
+
+    // Footer
+    "Heeft u vragen, problemen of feedback voor ons?" => "有问题，困难或反馈意见？",
+    "Contacteer ons via onze socials of gebruik ons " => "通过我们的社交媒体联系我们或使用我们的",
+    "contactformulier." => "联系表格。",
+    "Afbeelding via" => "圖片來自",
+    "op" => "在",
+
+    // Signup page
+    "Gebruikersnaam:" => "用户名：",
+    "E-mailadres:" => "电子邮件地址：",
+    "Wachtwoord:" => "密码：",
+    "Herhaal uw wachtwoord:" => "重复输入密码：",
+
+    // Login page
+    "Wachtwoord vergeten?" => "忘记密码？",
+
+    // Forgot password page
+    "Wachtwoord vergeten?" => "忘记密码？",
+    "Je kunt je wachtwoord opnieuw instellen, voer hiervoor je e-mailadres in en volg de daarna de instructies die je via de e-mail krijgt toegestuurd." => "您可以重置密码，请输入您的电子邮件地址，并按照通过电子邮件收到的说明进行操作。",
+    "Verstuur verzoek" => "发送请求",
+
+    // Contact form
+    "Contactformulier" => "联系表格",
+    "Voornaam:" => "名字：",
+    "Voer uw voornaam in" => "请输入您的名字",
+    "Achternaam:" => "姓氏：",
+    "Voer uw achternaam in" => "请输入您的姓氏",
+    "Voer uw e-mailadres in" => "请输入您的电子邮件地址",
+    "Voer een geldig e-mailadres in" => "请输入有效的电子邮件地址",
+    "Onderwerp:" => "主题：",
+    "Kies een onderwerp" => "选择一个主题",
+    "Suggestie / Feedback" => "建议/反馈",
+    "Klacht" => "投诉",
+    "Vraag over de applicatie" => "关于应用程序的问题",
+    "Overig" => "其他",
+    "Kies een onderwerp" => "选择一个主题",
+    "Bericht:" => "信息：",
+    "Schrijf hier uw bericht. (20 - 500 karakters)" => "在此处写下您的信息（20-500个字符）",
+    "Dit veld is verplicht. (20 - 500 karakters)" => "此字段为必填项（20-500个字符）",
+
+    // Account page
+    "Wijzig account" => "编辑帐户",
+    "Wijzig hier je gebruikersnaam, email en wachtwoord." => "在此处编辑您的用户名、电子邮件和密码。",
+    "Nieuw wachtwoord:" => "新密码：",
+    "Herhaal nieuw wachtwoord:" => "重复输入新密码：",
+    "Huidig wachtwoord:" => "当前密码：",
+    "Voer nieuw wachtwoord in" => "输入新密码",
+    "Voer huidig wachtwoord in" => "输入当前密码",
+    "Annuleer" => "取消",
+    "Opslaan" => "保存",
+    "Verwijder account" => "删除帐户",
+    "Bevestig verwijderen van account" => "确认删除帐户",
+    "Voer de 5 cijfers in ter bevestiging" => "输入5位数字以确认",
+    "Bevestig" => "确认",
+
+    // Userprofile Page
+    "Wijzig profiel" => "编辑个人资料",
+    "Recente afbeeldingen" => "最近的图片",
+    "Alle afbeeldingen weergeven" => "显示所有图片",
+
+
+    // Edit userprofile page
+    "Wijzig gebruikersprofiel" => "编辑用户个人资料",
+    "Wijzig hier de gegevens van het gebruikersprofiel dat aan andere Playm8 gebruikers wordt getoond." => "在此处编辑显示给其他 Playm8 用户的用户个人资料。",
+    "Wijzig profielfoto" => "编辑个人照片",
+    "Stad:" => "城市：",
+    "Voer uw stad in" => "输入您的城市",
+    "Land:" => "国家：",
+    "Voer uw land in" => "输入您的国家",
+    "Telefoonnummer:" => "电话号码：",
+    "Voer uw telefoonnummer in" => "输入您的电话号码",
+    "Geboortedatum:" => "出生日期：",
+    "Over mij koptekst:" => "关于我的标题：",
+    "Voer de titel van uw profielpagina in" => "输入您的个人资料页面的标题",
+    "Over mij:" => "关于我：",
+    "Vertel hier over de favoriete activiteiten, uitlaatplaatsen of andere leuke feitjes over jou en je huisdier! (20 - 255 karakters)" => "在这里分享关于您和您的宠物的喜爱活动、遛狗场所或其他有趣的事实！（20 - 255 个字符）",
+    "Dit veld is verplicht. (20 - 500 karakters)" => "此字段为必填项（20 - 500 个字符）",
+    "Verwijder gebruikersprofiel" => "删除用户个人资料",
+    "Bevestig verwijderen van gebruikersprofiel" => "确认删除用户个人资料"
 
 ];
