654c8eb569e74817e3df376efe2b1e8a852508fc
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-06-06 14:43:20 +0200

    created adminuploadCSV

--- includes/adminUploadCSVtoDB.inc.php
+++ includes/adminUploadCSVtoDB.inc.php
@@ -1,1 +1,28 @@
-// Script that takes the uploaded CSV file and instantiates the CSV controller that validates the CSV before submitting it to the DBphp
+<?php
+
+// Define the namespace of this script
+namespace Includes;
+
+// Include the autoload.php file composer automatically generates specifying PSR-4 autoload information set in composer.json
+require_once '../vendor/autoload.php';
+
+// Import classes this script depends on
+use Controller\CSVController;
+
+// Include script that handles the user pictures upload
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+$accountID = $_SESSION["auth_user"]["email"];
+
+if (isset($_POST["submit"])) {
+
+    // Grab the data of the file
+    $file = $_FILES["file"];
+
+    $CSVController = new CSVController($accountID);
+    $CSVController->uploadCSVtoDB($file);
+}
