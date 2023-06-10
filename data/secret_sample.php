d74dc227df14b79633db9e85287502a8b65f2699
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-06-10 19:57:18 +0200

    moved mailConfig class to own file

--- data/secret_sample.php
+++ data/secret_sample.php
@@ -19,21 +19,3 @@ class Secret
     const DB_PASSWORD = '';
     const DB_ADMIN = 'https://localhost/phpmyadmin';
 }
-
-// Class containing PHPMailer email credentials and configuration
-class mailConfig
-{
-    const CONFIG = [
-        'email' => [
-            'host' => 'smtp.office365.com',
-            'port' => 587,
-            'username' => 'donotreply-playm8@outlook.com',
-            'password' => '',
-            'SMTPSecure' => 'tls'
-        ]
-    ];
-
-    // Fill in your localhost APP URL in order to get right activation link in the accountDAO mailActivationCode method
-    // This should point to the playm8_mvc/ folder
-    const APP_URL = 'http://localhost/Coding_projects/GitHub%20Repositories/Playm8_mvc/';
-}
