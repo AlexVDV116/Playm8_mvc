e932889a27bbd76f3478333f916f0be87c8303a4
Alex Van der velden <a.vandervelden3@student.avans.nl>
2023-06-11 18:11:09 +0200

    created functions for matchProfiles functionality

--- dao/userProfileDAO.php
+++ dao/userProfileDAO.php
@@ -111,4 +111,102 @@ class userProfileDAO extends DAO
         ];
         $this->execute($sql, $args);
     }
+
+    // Function that gets all values from the userProfileID collumn in the userProfiles table
+    public function getAllUserProfileIDs(): array
+    {
+        $stmt = $this->prepare('CALL getAllUserProfileIDs();');
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    // Function that gets a random user
+    public function getRandomuserProfileID($myUserProfileID): mixed
+    {
+        // Use the getAllUserProfileIDs method to get a result set from the db containing all userProfileID's
+        $resultSet = $this->getAllUserProfileIDs();
+
+        // Use the array_column method to return the values from the userProfileID key in the result set
+        $allUserProfileIDs = array_column($resultSet, 0);
+
+        // Create a new empty currentProfile array which contains the current profile
+        $currentProfile = [];
+
+        // Randomize the order of the allUserProfileIDs array
+        shuffle($allUserProfileIDs);
+
+        // Remove the userProfileID of the current user from the allUserProfileIDs array
+        if (($key = array_search($myUserProfileID, $allUserProfileIDs)) !== false) {
+            unset($allUserProfileIDs[$key]);
+        }
+
+        // Get the userProfiles the current user has already likes
+        $resultSet2 = $this->getLikes($myUserProfileID);
+
+        // Use the array_column method to return the values from the userProfileID key in the result set
+        $likedUserProfiles = array_column($resultSet2, 0);
+
+        // Remove the already liked userProfiles from the allUserProfileIDs array
+        foreach ($likedUserProfiles as $likedUserProfile) {
+            if (($key = array_search($likedUserProfile, $allUserProfileIDs)) !== false) {
+                unset($allUserProfileIDs[$key]);
+            }
+        }
+
+        // Check if there are any userProfiles left in the allUserProfileIDs array
+        if (count($allUserProfileIDs) === 0) {
+            return false;
+        } else {
+
+
+            // Pop the last item of the allUserProfileIDs array and push it to the currentProfile array
+            array_push($currentProfile, array_pop($allUserProfileIDs));
+
+            // Get the userProfileID from the currentProfile array and get the UserProfile
+            $userProfile = $this->get($currentProfile[0]);
+
+            // Remove the userProfileID from the currentProfile array
+            array_pop($currentProfile);
+
+            // Return the userprofile
+            return $userProfile;
+        }
+    }
+
+    // Function that adds a record to the likes table with the userProfileIDs of the liker and liked
+    public function addLike($liker, $liked): void
+    {
+        $stmt = $this->prepare('CALL addLike(?, ?);');
+        $stmt->execute([$liker, $liked]);
+        $stmt->closeCursor();
+    }
+
+    public function getLikes($userProfileID): array
+    {
+        $stmt = $this->prepare('CALL getLikes(?);');
+        $stmt->execute([$userProfileID]);
+        $result = $stmt->fetchAll();
+        $stmt->closeCursor();
+
+        return $result;
+    }
+
+    // Function that checks if the liker and liked have a record in the matches table, returns bool
+    public function checkMatch($liker, $liked): bool
+    {
+        $stmt = $this->prepare('CALL checkMatch(?, ?);');
+        $stmt->execute([$liker, $liked]);
+        $result = $stmt->fetch();
+        $stmt->closeCursor();
+
+        $resultCheck = null;
+        if ($result) {
+            $resultCheck = true;
+        } else {
+            $resultCheck = false;
+        }
+        return $resultCheck;
+    }
 }
